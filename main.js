2dfeb14c5c8ba2eb791b78f898f90fbc9e436b8d
maddogb <53348007+maddogb@users.noreply.github.com>
2019-07-26 11:18:31 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+document.getElementById("btn1").addEventListener("click", myName)
+document.getElementById("btn2").addEventListener("click", mySchool)
+document.getElementById("btn3").addEventListener("click", myCareer)
+document.getElementById("btn4").addEventListener("click", myHobbies)
+
+
+function myName() {
+   
+    document.getElementById("name").innerHTML = "My name is Madison but you can call me by my nickname Maddy or Mad Dog. i was born on July 14 so my zodiac sign is Cancer. yikes!!";
+} 
+
+function mySchool() {
+    document.getElementById("school").innerHTML = "I am a senior at Harmony School of Endeavor. Harmony is a charter school that teachers grades k-12th.";
+}
+
+function myCareer() {
+    document.getElementById("career").innerHTML = "For the time being i am currently employed at Kumon. At kumon i help teach little kids reading , writing and math. I chose to work at kumon to have experience working and helping kids because in the future I want to become a pediatrician ";
+}
+
+function myHobbies() {
+    document.getElementById("hobbies").innerHTML = "I like to play different instruments but mainly flute and i like to sing . I also like to play volleyball.";
+}
