2f0b28ff5d4ff7015505a13950fd53579d709551
maddogb <53348007+maddogb@users.noreply.github.com>
2019-07-26 13:20:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ function myName() {
 } 
 
 function mySchool() {
-    document.getElementById("school").innerHTML = "I am a senior at McNeil High School";
+    document.getElementById("school").innerHTML = "I am a senior at Harmony School of Endeavor.";
 }
 
 function myCareer() {
-    document.getElementById("career").innerHTML = "For the time being i am currently a tutor of kids. i tutor little kids in reading , writing and math. I chose to be a tutor because i wanted to have experience working and helping kids because in the future I want to become a pediatrician ";
+    document.getElementById("career").innerHTML = "For the time being I am currently employed at kumon. I help little kids with reading, writing , and math. I chose to be work at kumon because i wanted to have an experience working and helping kids because in the future I want to become a pediatrician ";
 }
 
 function myHobbies() {
-    document.getElementById("hobbies").innerHTML = "I like to play different instruments but mainly flute and i like to sing . I also like to play volleyball.";
+    document.getElementById("hobbies").innerHTML = "I like to play different instruments but mainly flute and i like to sing . I also like to play volleyball.I first started playing the flute in the 6th grate.";
 }
